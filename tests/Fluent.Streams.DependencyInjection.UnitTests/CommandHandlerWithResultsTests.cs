f8db8b02d03e7c1112d687d0c9e91f6f22f84de3
pomianowski <13592821+pomianowski@users.noreply.github.com>
2026-07-04 16:30:42 +0200

    chore: Initialize test class

---
+++ tests/Fluent.Streams.DependencyInjection.UnitTests/CommandHandlerWithResultsTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Fluent.Streams.DependencyInjection.UnitTests;
+
+public sealed class CommandHandlerWithResultsTests
+{
+    [Fact]
+    public void D()
+    {
+        true.Should().BeTrue();
+    }
+}
